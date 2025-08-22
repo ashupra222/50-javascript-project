12677a1a3aef450c8de65aae99e8fda084128935
ashutosh <ashupra222@gmail.com>
2025-08-22 13:19:07 +0530

    Update in Project 2

--- project 2/script.js
+++ project 2/script.js
@@ -1,15 +1,15 @@
 function next() {
     const progressBar = document.getElementsByClassName('progress-fill')[0];
     let currentWidth = parseInt(progressBar.style.width) || 0;
-    currentWidth += 30; 
+    currentWidth += 33; 
     progressBar.style.width = currentWidth + '%';
-    if (currentWidth == 30) {
+    if (currentWidth == 33) {
         document.getElementById('progressText2').classList.add('completed');
     }
-    else if (currentWidth == 60) {
+    else if (currentWidth == 66) {
         document.getElementById('progressText3').classList.add('completed');
     }
-    else if (currentWidth == 90) {
+    else if (currentWidth == 99) {
         document.getElementById('progressText4').classList.add('completed');
     }
     if (currentWidth == 90) {
@@ -24,22 +24,22 @@ function next() {
 function previous() {
     const progressBar = document.getElementsByClassName('progress-fill')[0];
     let currentWidth = parseInt(progressBar.style.width) || 0;
-    if (currentWidth == 30) {
+    if (currentWidth == 33) {
         document.getElementById('progressText2').classList.remove('completed');
     }
-    else if (currentWidth == 60) {
+    else if (currentWidth == 66) {
         document.getElementById('progressText3').classList.remove('completed');
     }
-    else if (currentWidth == 90) {
+    else if (currentWidth == 99) {
         document.getElementById('progressText4').classList.remove('completed');
     }
-    currentWidth -= 30;
+    currentWidth -= 33;
     progressBar.style.width = currentWidth + '%';
     if (currentWidth == 0) {
         document.getElementById('previous').disabled = true;
         document.getElementById('next').disabled = false;
     }
-    if (currentWidth < 90) {
+    if (currentWidth < 99) {
         document.getElementById('next').disabled = false;
     }
 }
