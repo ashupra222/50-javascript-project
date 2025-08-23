201a103cf18de97f897acc4453e08b30dd97cbf5
ashutosh <ashupra222@gmail.com>
2025-08-23 16:46:43 +0530

    Project 5: Blurry Loading

--- Project 4/script.js
+++ Project 4/script.js
@@ -3,7 +3,6 @@ let searchbtn = document.querySelector('.search-btn');
 let searchinput = document.querySelector('.search-input');
 
 searchbtn.addEventListener('click',()=>{
-    console.log('clicked');
     searchbox.classList.toggle('active');
     searchinput.focus();
 });
