7fcf1ad6f2eef8035bfa8eeac825381f4172dfaf
ashutosh <ashupra222@gmail.com>
2025-08-24 22:38:45 +0530

    Project 11: Event KeyCode

---
+++ Project 11/script.js
@@ -0,0 +1,17 @@
+let insert = document.getElementById('insert')
+window.addEventListener('keypress', (e) => {
+    insert.innerHTML = `
+        <div class="key">
+            ${e.key === ' '? 'Space': e.key}
+            <small>event.key</small>
+        </div>
+        <div class="key">
+            ${e.keyCode}
+            <small>event.keyCode</small>
+        </div>
+        <div class="key">
+            ${e.code}
+            <small>event.code</small>
+        </div>
+    `
+})
